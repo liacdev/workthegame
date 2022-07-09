1ec54efe4173fed5d39e81c8a917ae2ad8976539
liacdev <jagoblacksky@gmail.com>
2022-07-09 17:29:57 -0500

    Update text components

--- Assets/Scripts/WorkTheGame.cs
+++ Assets/Scripts/WorkTheGame.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class WorkTheGame : MonoBehaviour
 {
+
+    [SerializeField] Text textComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = "Get back to work!";
     }
 
     // Update is called once per frame
