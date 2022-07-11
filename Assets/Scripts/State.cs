bdddb2ad0bcdfe9c162e692fe6f2e0e5cff1b523
liacdev <jagoblacksky@gmail.com>
2022-07-10 19:23:29 -0500

    Added States and TMP Text

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "State")]
+
 public class State : ScriptableObject
 {
-    [TextArea(10, 14)] [SerializeField] string storyText;
+    [TextArea(18, 14)] [SerializeField] string storyText;
+    [TextArea(4, 14)] [SerializeField] string finePrint;
+
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
+    }
+
+    public string GetFinePrint()
+    {
+        return finePrint;
+    }
+
+
+    public State[] GetNextStates(){
+        return nextStates;
     }
 
 }
--- Assets/Scripts/WorkTheGame.cs
+++ Assets/Scripts/WorkTheGame.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class WorkTheGame : MonoBehaviour
 {
 
-    [SerializeField] Text textComponent;
+    [SerializeField] TMP_Text textComponent;
+    [SerializeField] TMP_Text textComponent2;
     [SerializeField] State startingState;
+
 
     State state;
 
@@ -17,11 +19,43 @@ public class WorkTheGame : MonoBehaviour
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
+        textComponent2.text = state.GetFinePrint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
     }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+       
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+       
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextStates[3];
+        }
+        
+        textComponent.text = state.GetStateStory();
+        textComponent2.text = state.GetFinePrint();
+
+    }
+    
+
 }
